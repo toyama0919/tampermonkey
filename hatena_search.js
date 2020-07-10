db657c44e9469ef47300dda8fc4618c2014a307e
Hiroshi Toyama <toyama0919@gmail.com>
2020-07-10 19:21:31 +0900

    fix url.

--- hatena_search.js
+++ hatena_search.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://b.hatena.ne.jp/search/text?safe=on&sort=recent&q=*
+// @match        https://b.hatena.ne.jp/search/text?safe=on&sort=recent&q=*
 // @grant        none
 // ==/UserScript==
 function filterHaveInnerHTML(argElements) {
