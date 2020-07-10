4322b35e0f1b809fc7916064dfb1c71e0bea572c
Hiroshi Toyama <toyama0919@gmail.com>
2020-07-10 18:59:23 +0900

    add hatena.

---
+++ hatena.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         hatena_top
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://b.hatena.ne.jp/
+// @match        https://b.hatena.ne.jp/hotentry/it
+// @match        https://b.hatena.ne.jp/entrylist/it
+// @grant        none
+// ==/UserScript==
+function filterHaveInnerHTML(argElements) {
+  var i;
+  var elms = [];
+  for(i=0;i < argElements.length;i++){
+      var element = argElements[i];
+      if(element.innerHTML !== "") {
+          elms.push(element);
+      }
+  }
+  return elms;
+}
+
+function paintSingle(number, argElements) {
+  var nowElement = argElements[number];
+  nowElement.focus();
+}
+
+var elements = filterHaveInnerHTML(document.querySelectorAll("h3.entrylist-contents-title>a"));
+var now = -99;
+
+document.addEventListener("keydown", function(event) {
+  elements = filterHaveInnerHTML(document.querySelectorAll("h3.entrylist-contents-title>a"));
+  var id_str = document.activeElement.id;
+  if(now == -99) {
+    if(event.keyCode == 38 || event.keyCode == 40) {
+      now = 0;
+      paintSingle(now, elements);
+    }
+  } else {
+    if (event.keyCode == 38) {
+      if (now > 0) {
+        now = now - 1;
+        paintSingle(now, elements);
+      }
+    }
+    if (event.keyCode == 40) {
+      if (now < elements.length - 1) {
+        now = now + 1;
+        paintSingle(now, elements);
+      }
+    }
+  }
+});
