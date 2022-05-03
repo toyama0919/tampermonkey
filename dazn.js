6449739a87e50bf9d50016b865030cee14563b09
Hiroshi Toyama <toyama0919@gmail.com>
2022-05-03 23:59:42 +0900

    add dazn.

---
+++ dazn.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         dazn
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.dazn.com/ja-JP/fixture/ContentId:*
+// @grant        none
+// ==/UserScript==
+
+document.addEventListener("keydown", function(event) {
+  if(event.keyCode == 70) {
+    var elm = document.getElementsByClassName("fullscreen___fullscreen___1OXBx")[0];
+    elm.click();
+  }
+
+  if(event.keyCode == 72) {
+    var elm_switch = document.getElementsByClassName("video-switcher__label___1kJP3")[0];
+    elm_switch.click();
+  }
+});
