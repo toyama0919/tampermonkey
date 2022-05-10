ec274473b4fafb0b4ea2dd0f86bd177b0fa25c16
Hiroshi Toyama <toyama0919@gmail.com>
2022-05-10 22:09:22 +0900

    fix dazn.

--- dazn.js
+++ dazn.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.dazn.com/ja-JP/fixture/ContentId:*
+// @match        https://www.dazn.com/ja-JP/schedule
 // @grant        none
 // ==/UserScript==
 
@@ -15,5 +16,11 @@ document.addEventListener("keydown", function(event) {
   } else if(event.code == "KeyH") {
     var elm_switch = document.getElementsByClassName("video-switcher__label___1kJP3")[0];
     elm_switch.click();
+  } else if(event.code == "KeyA") {
+    var elm_rewind = document.getElementsByClassName("playbackButtons___playback-button___2bb6j")[0];
+    elm_rewind.click();
+  } else if(event.code == "KeyD") {
+    var elm_forward = document.getElementsByClassName("playbackButtons___playback-button___2bb6j")[2];
+    elm_forward.click();
   }
 });
