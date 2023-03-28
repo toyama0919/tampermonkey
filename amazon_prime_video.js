cc9a85fe8a2ba8ac80be363b9a7caee71266a7c7
Hiroshi Toyama <toyama0919@gmail.com>
2023-03-28 21:15:41 +0900

    add prime video.

---
+++ amazon_prime_video.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         amazon_prime_video
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amazon.co.jp/gp/video/detail/*
+// @grant        none
+// ==/UserScript==
+
+document.addEventListener("keydown", function(event) {
+  // 各種操作
+  if(event.code == "KeyF") {
+    var elm = document.getElementsByClassName("atvwebplayersdk-fullscreen-button")[0];
+    elm.click();
+  } else if(event.code == "KeyS") {
+  } else if(event.code == "KeyC") {
+    var elm_switch = document.getElementsByClassName("subtitles_fll_open_close_button_container")[0];
+    elm_switch.click();
+  } else if(event.code == "KeyA") {
+    const ke = new KeyboardEvent('keydown', {
+      bubbles: true, cancelable: true, keyCode: 69
+    });
+    document.body.dispatchEvent(ke);
+  }
+});
